9087cb61d9803f2a7dec7d18dca0e72922487f50
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-11-23 23:43:51 +0200

    added model "news"

---
+++ models/news.js
@@ -0,0 +1,51 @@
+import Joi from "joi";
+import { Schema, model } from "mongoose";
+
+import { handleUpdateValidate, handleMongooseError } from "../helpers/index.js";
+
+const newsSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: [true, "Title is required"],
+		},
+		description: {
+			type: String,
+			default: "",
+		},
+		imageURL: {
+			type: [
+				new Schema({
+					url: { type: String, required },
+				}),
+			],
+		},
+		youtubeURL: {
+			type: String,
+			default: "",
+		},
+		owner: {
+			type: Schema.Types.ObjectId,
+			ref: "user",
+			required: true,
+		},
+	},
+	{ versionKey: false, timestamps: true },
+);
+
+newsSchema.pre("findOneAndUpdate", handleUpdateValidate);
+
+newsSchema.post("save", handleMongooseError);
+
+newsSchema.post("findOneAndUpdate", handleMongooseError);
+
+const News = model("news", newsSchema);
+
+const newsSchemaJoi = Joi.object({
+	title: Joi.string().required(),
+	description: Joi.string(),
+	file: Joi.string().allow("").optional(),
+	youtubeURL: Joi.string(),
+});
+
+export { News, newsSchemaJoi };
